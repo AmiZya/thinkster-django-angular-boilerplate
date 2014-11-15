a321d398afff084e3d569b4a7bbd9b78cb04b16e
Amine Zyad <amizya@gmail.com>
2014-11-15 18:49:51 +0000

    Implement user profile serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 
 from rest_framework import serializers
 
+from authentication.models import UserProfile
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -19,3 +21,32 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
 
         return user
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='pk', read_only=True)
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.CharField(source='user.email', required=False)
+    first_name = serializers.CharField(source='user.first_name', required=False)
+    last_name.serializers.CharField(source='user.last_name', required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = (
+            'id', 'username', 'email', 'first_name', 'last_name', 'tagline', 'created_at', 'updated_at',
+        )
+        read_only_fields = ('created_at', 'updated_at',)
+
+    def restore_object(self, attrs, instance=None):
+        profile = super(UserProfileSerializer, self).restore_object(attrs, instance)
+
+        if profile:
+            user = profile.user
+
+            user.email = attrs.get('user.email', user.email)
+            user.first_name = attrs.get('user.first_name', user.first_name)
+            user.last_name = attrs.get('user.last_name', user.last_name)
+
+            user.save()
+
+        return profile
