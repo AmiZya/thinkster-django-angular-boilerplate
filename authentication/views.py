1d9bd5cdbfb6376341c81481dfe529bb8da0b873
Amine Zyad <amizya@gmail.com>
2014-11-15 23:27:41 +0000

    Created login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from rest_framework import generics
+from rest_framework import status, views
+from rest_framework import Response
 from authentication.serializers import UserSerializer
+import json
 
 
 class UserCreateView(generics.CreateAPIView):
@@ -8,3 +12,26 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        username = data.get('username', None)
+        password = data.get('password', None)
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                serialized = UserSerializer(user)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'error': 'Awkward! Your account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'error': 'Looks like your username or password is wrong. :('
+            }, status=status.HTTP_400_BAD_REQUEST)
